656b47b5a24d6ab1e7c47b2777911dbed3b434d0
Wenqi He <hewenqi96@gmail.com>
2019-09-07 05:08:52 -0400

    Initiate requests from front end

--- backend/index.js
+++ backend/index.js
@@ -3,59 +3,35 @@ const express = require('express');
 const { parseString: parseXML } = require('xml2js');
 const { getUserInfoById } = require('./user');
 
+const CALLBACK_URL = `http://openeval.gatech.edu:4200/home`;
 const app = express();
 
-const CALLBACK_URL = 'http://openeval.gatech.edu/landing';
-
-app.get('/login', (req, res) => {
-  res.redirect(`https://login.gatech.edu/cas/login?service=${
-    encodeURIComponent(CALLBACK_URL)
-  }`);
-});
-
-app.get('/landing', (req, res) => {
+app.get('/validate', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://openeval.gatech.edu:4200');
   let ticket = req.query.ticket;
-  console.log('Got ticket:', ticket);
-
   let validation_url = `https://login.gatech.edu/serviceValidate?service=${
     encodeURIComponent(CALLBACK_URL)
-  }&ticket=${
-    ticket
-  }`;
+  }&ticket=${ticket}`;
 
   request(validation_url, (err, response, body) => {
     parseXML(body, (err, result) => {
       if (err) {
-        console.log(err);
+        res.send({ loggedIn: false, reason: err });
         return;
       }
       if (result['cas:serviceResponse']['cas:authenticationSuccess']) {
-
         let username = result['cas:serviceResponse']['cas:authenticationSuccess'][0]['cas:user'][0];
-        getUserInfoById(username).then(info => {
-          // console.log(info)
-          res.send(`
-            <h1>${info[0].displayName.split(',').reverse().join(' ')}</h1>
-            <h2>${info[0].title}, ${info[0].ou}</h2>
-            ${info[0].postOfficeBox ? `<p>P.O. Box: ${info[0].postOfficeBox}</p>` : ''}
-            ${info[0].telephoneNumber ? `<p>Phone Number: ${info[0].telephoneNumber}</p>` : ''}
-            <b>Raw Data:</b>
-            <pre>${JSON.stringify(info, undefined, 2)}</pre>
-          `)
+        getUserInfoById(username).then(data => {
+          res.send({ loggedIn: true, info: data[0] });
         }, (reason) => {
-          console.log(reason);
-          res.send(`<p>${reason}</p>`);
+          res.send({ loggedIn: false, reason });
         })
 
       } else {
-        res.send(`
-          <h1>Please Log In</h1>
-          <a href="/login">Go to Login Page</a>
-        `);
+        res.send({ loggedIn: false, reason: '?' });
       }
     });
   });
 });
 
-app.get('*', (req, res) => res.redirect('/landing'));
-app.listen(80, () => console.log('Listening on port 80'));
+app.listen(4201, () => console.log(`Listening on port 4201`));
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+// import { parseString } from 'xml2js';
+// const { parseXML } = parseString;
+
+const CALLBACK_URL = `http://openeval.gatech.edu:4200/home`;
+const API_SERVER_URL = `http://openeval.gatech.edu:4201`;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'open-eval';
+  loggedIn = false;
+  private _info = null;
 
   constructor() { }
 
-  ngOnInit() {
+  get userInfo() {
+    return JSON.stringify(this._info, undefined, 2);
   }
+
+  ngOnInit() {
+    let url = new URL(window.location.href);
+    let params = new URLSearchParams(url.search);
+    if (!params.has('ticket')) return;
+
+    let ticket = params.get('ticket');
+
+    fetch(`${API_SERVER_URL}/validate?ticket=${ticket}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.loggedIn = json.loggedIn;
+        this._info = json.info;
+      }); 
+  }
+
+  login() {
+    window.location.href = `https://login.gatech.edu/cas/login?service=${
+      encodeURIComponent(CALLBACK_URL)
+    }`;
+  }
+
 }
