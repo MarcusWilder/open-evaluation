974d28277ab4873c03dfbceaf28c65da3f27dc34
MarcusWilder <marcusnwilder@gmail.com>
2019-09-08 11:07:58 -0400

    Global Nav tabs now switch focus, active tab is emitted as component output, and button text now customizable

--- src/app/components/global-navigation/global-navigation.component.ts
+++ src/app/components/global-navigation/global-navigation.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'app-global-navigation',
@@ -7,13 +12,29 @@ import { Component, Input } from '@angular/core';
 })
 export class GlobalNavigationComponent {
 
-  @Input() appName: string;
-  tabCollection: object[];
-
+  @Input() name: string;
+  @Input() buttonText: string;
   @Input() set tabs(collection: string[]) {
     this.tabCollection = [];
     for (let ind = 0; ind < collection.length; ind++) {
-      this.tabCollection.push({name: collection[ind], active: false});
+      if (ind === 0) {
+        this.tabCollection.push({name: collection[ind], active: true});
+      } else {
+        this.tabCollection.push({name: collection[ind], active: false});
+      }
     }
+    this.activeTab.emit(this.tabCollection[this.currActive]['name']);
+  }
+
+  @Output() activeTab = new EventEmitter<string>();
+
+  currActive = 0;
+  tabCollection: object[];
+
+  changeActive(ind: number) {
+    this.tabCollection[this.currActive]['active'] = false;
+    this.tabCollection[ind]['active'] = true;
+    this.currActive = ind;
+    this.activeTab.emit(this.tabCollection[this.currActive]['name']);
   }
 }
