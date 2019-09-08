366affaa29c1aebeb4b0c5d873af4e4483cab061
Wenqi He <hewenqi96@gmail.com>
2019-09-08 17:22:54 -0400

    Add session cookies

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,29 @@
 const request = require('request');
 const express = require('express');
+var cookieParser = require('cookie-parser');
+const uniqueString = require('unique-string');
 const { parseString: parseXML } = require('xml2js');
 const { getUserInfoById } = require('./user');
 
-const CALLBACK_URL = `http://openeval.gatech.edu:4200/home`;
+const CALLBACK_URL = `http://openeval.gatech.edu:4200/dashboard`;
 const app = express();
+const sessions = new Map();
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://openeval.gatech.edu:4200');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // console.log('Cookies:', req.cookies);
+  next();
+});
 
 app.get('/validate', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://openeval.gatech.edu:4200');
   let ticket = req.query.ticket;
+  if (!ticket) {
+    res.send({ loggedIn: false, reason: 'No Ticket' });
+    return;
+  }
   let validation_url = `https://login.gatech.edu/serviceValidate?service=${
     encodeURIComponent(CALLBACK_URL)
   }&ticket=${ticket}`;
@@ -19,19 +34,38 @@ app.get('/validate', (req, res) => {
         res.send({ loggedIn: false, reason: err });
         return;
       }
-      if (result['cas:serviceResponse']['cas:authenticationSuccess']) {
-        let username = result['cas:serviceResponse']['cas:authenticationSuccess'][0]['cas:user'][0];
+      if (sessions.has(req.cookies.SESSION)) {
+        let username = sessions.get(req.cookies.SESSION);
         getUserInfoById(username).then(data => {
           res.send({ loggedIn: true, info: data[0] });
         }, (reason) => {
           res.send({ loggedIn: false, reason });
-        })
-
-      } else {
-        res.send({ loggedIn: false, reason: '?' });
+        });
+        return;
       }
+      if (result['cas:serviceResponse']['cas:authenticationSuccess']) {
+        let username = result['cas:serviceResponse']['cas:authenticationSuccess'][0]['cas:user'][0];
+        const session = uniqueString();
+        sessions.set(session, username);
+        res.cookie('SESSION', session);
+        getUserInfoById(username).then(data => {
+          res.send({ loggedIn: true, info: data[0] });
+        }, (reason) => {
+          res.send({ loggedIn: false, reason });
+        });
+        return;
+      }
+      res.send({ loggedIn: false, reason: result });
     });
   });
 });
+
+app.get('/logout', (req, res) => {
+  let session = req.cookies.SESSION;
+  if (sessions.has(session)) {
+    sessions.delete(session);
+  }
+  res.send();
+})
 
 app.listen(4201, () => console.log(`Listening on port 4201`));
