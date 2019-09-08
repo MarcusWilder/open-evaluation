b5fdef50d39c8f174d07718cb304e8db8e213c11
cclark931 <cclark931@gmail.com>
2019-09-08 14:32:49 -0400

    merge master in, adds global nav component to welcome page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  title = 'open-eval';
 
   constructor() { }
+
+  title = 'open-eval';
+  tabs = ['Home', 'Features', 'Resources'];
+  activeTab: string;
 
   ngOnInit() {
   }
