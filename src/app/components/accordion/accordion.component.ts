5cade28685497baffe232237e0cdd594cc6e7093
benjavaisberg <benjaminvaisberg96@gmail.com>
2019-09-15 17:07:10 -0400

    initial commit

---
+++ src/app/components/accordion/accordion.component.ts
@@ -0,0 +1,16 @@
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output
+  } from '@angular/core';
+
+  @Component({
+      selector: "app-accordion",
+      templateUrl: "./accordion.component.html",
+      styleUrls: ["./button-group.component.css"]
+  })
+
+  export class AccordionComponent {
+      
+  }
