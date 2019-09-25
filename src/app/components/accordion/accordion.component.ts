39a4afbd98231135707a543c9300853edbf34817
benjavaisberg <benjaminvaisberg96@gmail.com>
2019-09-24 21:32:05 -0400

    initial commit

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.css']
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent {
 
-  constructor() { }
+  tabCollection: object[];
 
-  ngOnInit() {
+  @Input() header:string;
+  @Input() callback: () => void;
+  @Input() set tabs(collection: string[]) {
+
   }
-
 }
