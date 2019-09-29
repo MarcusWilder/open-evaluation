ca39e9adeb58a885c0da83d6cd23966baa96b64f
MarcusWilder <marcusnwilder@gmail.com>
2019-09-29 16:37:40 -0400

    Fixed click event calling errors

--- src/app/components/button-group/button-group.component.ts
+++ src/app/components/button-group/button-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
+
 import { Button } from '@src/app/types/button-group-types';
 
 @Component({
@@ -9,13 +10,13 @@ import { Button } from '@src/app/types/button-group-types';
 })
 export class ButtonGroupComponent { // Will eventually need to incorporate overlay for dropdown
 
-  @Input() set buttons(value: Button[]) {
-    if (value.length > 3) {
+  @Input() set buttons(collection: Button[]) {
+    if (collection.length > 3) {
       this.overflow = true;
-      this.innerButtons = value.slice(3, value.length);
-      this.outerButtons = value.slice(0, 3);
+      this.innerButtons = collection.slice(3, collection.length);
+      this.outerButtons = collection.slice(0, 3);
     } else {
-      this.outerButtons = value;
+      this.outerButtons = collection;
     }
 
   }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ToastService } from '@src/app/services/toast/toast.service';
+
 import { Button } from '@src/app/types/button-group-types';
+import { ToastService } from '@src/app/services/toast/toast.service';
 
 @Component({
   selector: 'app-test',
@@ -11,20 +12,20 @@ export class TestComponent {
 
   constructor(private toastService: ToastService) {}
 
+  activeTab: string;
   buttons: Button[] = [
     { type: 'destructive', content: 'Discard', onClick: () => alert('Discarded!') },
     { type: 'brand', content: 'Save' , onClick: () => alert('Saved!') },
     { type: 'success', content: 'Submit' }
   ];
-  tabs = ['Home', 'Features', 'Resources'];
-  activeTab: string;
   firstName: string;
   lastName: string;
   rankings = [1, 2, 3, 4, 5];
   selection: unknown;
+  tabs = ['Home', 'Features', 'Resources'];
 
-  submit() {
-    this.toastService.open('Survey Response Received', 'Thank you for your feedback', 'success');
-  }
+  // submit() {
+  //   this.toastService.open('Survey Response Received', 'Thank you for your feedback', 'success');
+  // }
 
 }
