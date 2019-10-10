8bceb284bac07fb1e0179b363a2290a8cbfca73c
Wenqi He <hewenqi96@gmail.com>
2019-10-10 04:10:23 -0400

    Add CTL questions

--- src/app/mock-data/mock-questions.ts
+++ src/app/mock-data/mock-questions.ts
@@ -1,7 +1,7 @@
 import { Question } from '../objects/question';
 import { QuestionType } from '../objects/question-type';
 
-export const QUESTIONS: Question[] = [
+export const DEFAULT_QUESTIONS: Question[] = [
     {
         id: 1,
         type: QuestionType.multipleChoice,
@@ -39,3 +39,46 @@ export const QUESTIONS: Question[] = [
         baseQuestion: 'Any additional comments about this course:'
     }
 ];
+
+export const CTL_QUESTIONS: Question[] = [
+    {
+        id: 1,
+        type: QuestionType.ranking,
+        baseQuestion: 'Class sessions are interesting'
+    },
+    {
+        id: 2,
+        type: QuestionType.ranking,
+        baseQuestion: 'The workload is reasonable'
+    },
+    {
+        id: 3,
+        type: QuestionType.ranking,
+        baseQuestion: 'I know what I need to do to succeed in this course'
+    },
+    {
+        id: 4,
+        type: QuestionType.ranking,
+        baseQuestion: 'I am satisfied with my decision to take this course'
+    },
+    {
+        id: 5,
+        type: QuestionType.ranking,
+        baseQuestion: 'I am comfortable approaching the instructor to talk about this course'
+    },
+    {
+        id: 6,
+        type: QuestionType.ranking,
+        baseQuestion: 'I would recommend this class to a friend'
+    },
+    {
+        id: 7,
+        type: QuestionType.freeResponse,
+        baseQuestion: 'What are the strongest features of this course and of my teaching? In other words, what contributes most to your learning?'
+    },
+    {
+        id: 8,
+        type: QuestionType.freeResponse,
+        baseQuestion: 'What specific suggestions do you have for changes that I can make to improve the course and/or how it is taught?'
+    }
+]
