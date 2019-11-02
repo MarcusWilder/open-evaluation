1060e020a7b5a4fe3fce1396ee59cffe28b4f6a6
Wenqi He <hewenqi96@gmail.com>
2019-11-02 05:31:46 -0400

    Get user role

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,34 @@ const app = express();
 const TOKEN = '2096~qSLzU7BxpYDDiPppntkyBADaMxUl9Rjrtc8g6pKrhIeOcTf6intMwZJHv6vTp7YY';
 
 app.get('/user', async (req, res) => {
-  let raw = await fetch(`
-    https://gatech.instructure.com/api/v1/users/self`,
-    { headers: { 'Authorization': 'Bearer ' + TOKEN } }
-  );
-  let text = await raw.text();
   try {
+    // Basic info
+    let raw = await fetch(`
+      https://gatech.instructure.com/api/v1/users/self`,
+      { headers: { 'Authorization': 'Bearer ' + TOKEN } }
+    );
+    let text = await raw.text();
     let data = JSON.parse(text.slice(text.indexOf(';') + 1));
     if (data.errors) throw data.errors;
     const { id, name } = data;
-    res.send({ id, name });
+    
+    // Determine user role
+    raw = await fetch(
+      `https://gatech.instructure.com/api/v1/users/self/enrollments`,
+      { headers: { 'Authorization': 'Bearer ' + TOKEN } }
+    );
+    text = await raw.text();
+    data = JSON.parse(text.slice(text.indexOf(';') + 1));
+    if (data.errors) throw data.errors;
+    let studentEnrollmentCount = 0;
+    for (let enrollment of data) {
+      if (enrollment.type === 'StudentEnrollment') {
+        studentEnrollmentCount++;
+      }
+    }
+    const role = studentEnrollmentCount > data.length / 2 ? 'student' : 'professor';
+
+    res.send({ id, name, role });
   } catch (err) {
     res.send(err);  
   }
