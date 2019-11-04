570b87b5a455a4a64b17f5aec2d523eef95b8fc8
Wenqi He <hewenqi96@gmail.com>
2019-11-04 06:23:14 -0500

    Error handling

--- backend/get-cookie.js
+++ backend/get-cookie.js
@@ -3,27 +3,33 @@ const puppeteer = require('puppeteer');
 const COOKIE_NAME = 'canvas_session';
 
 exports.getCookie = async function(username, password) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://gatech.instructure.com/');
-    
-    await page.waitForSelector('#password', { timeout: 5000 });
-    await page.type('#username', username); 
-    await page.type('#password', password);
-    await page.click('[type="submit"]');
-    
-    await page.waitForSelector('#duo_iframe', { timeout: 5000 })
-    const iframe = await page.$('#duo_iframe');
-    const duoFrame = await iframe.contentFrame();
-    await duoFrame.waitForNavigation();
-    await duoFrame.waitForSelector('#login-form [type="submit"]');
-    await duoFrame.evaluate(() => {
-        const pushButton = document.querySelector('#login-form [type="submit"]');
-        pushButton.click();
-    });
-    await page.waitForSelector('#application'); // Wait for Canvas webpage to load
-    const cookies = await page.cookies();
-    const entry = cookies.find(c => c.name == COOKIE_NAME);
-    if (entry) return entry.value;
+    try {
+        const browserPromise = puppeteer.launch();
+        const browser = await browserPromise;
+        const page = await browser.newPage();
+        await page.goto('https://gatech.instructure.com/');
+        
+        await page.waitForSelector('#password', { timeout: 5000 });
+        await page.type('#username', username); 
+        await page.type('#password', password);
+        await page.click('[type="submit"]');
+        
+        await page.waitForSelector('#duo_iframe', { timeout: 5000 })
+        const iframe = await page.$('#duo_iframe');
+        const duoFrame = await iframe.contentFrame();
+        await duoFrame.waitForNavigation();
+        await duoFrame.waitForSelector('#login-form [type="submit"]');
+        await duoFrame.evaluate(() => {
+            const pushButton = document.querySelector('#login-form [type="submit"]');
+            pushButton.click();
+        });
+        await page.waitForSelector('#application'); // Wait for Canvas webpage to load
+        const cookies = await page.cookies();
+        const entry = cookies.find(c => c.name == COOKIE_NAME);
+        if (entry) return entry.value;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
     return null;
 }
